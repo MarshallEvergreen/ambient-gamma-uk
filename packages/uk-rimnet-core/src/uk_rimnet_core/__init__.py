a348e711ef58605019901a9c2f63c6deb57d3e8f
Abie Marshall <abiemarshall94@gmail.com>
2026-06-02 20:06:11 +0100

    rename file

--- packages/uk-rimnet-core/src/uk_rimnet_core/__init__.py
+++ packages/uk-rimnet-core/src/uk_rimnet_core/__init__.py
@@ -1,7 +1,7 @@
 """uk-rimnet-core: access and process UK ambient gamma radiation monitoring data."""
 
-from uk_rimnet_core._dataset import build_dataset
 from uk_rimnet_core.client import Client
+from uk_rimnet_core.dataset import build_dataset
 from uk_rimnet_core.location_registry import LocationRegistry
 
 __all__ = ["Client", "LocationRegistry", "build_dataset"]
